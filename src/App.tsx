7447d4b2a940cccb495fd92bc9bc3d2e46bd6ac0
_ <_@_.com>
2023-12-12 23:03:21 +0800

    feat: render marker

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,25 @@
 import { Box } from '@chakra-ui/react'
 import { useEffect, useRef, useState } from 'react'
+import { Atm, listAtm } from './services/listAtm'
+import { Marker } from './Marker'
 const center = { lat: 22.3316025, lng: 114.12776 }
 const zoom = 12
 export const App = () => {
   const ref = useRef<HTMLDivElement>(null)
   const [map, setMap] = useState<google.maps.Map>()
+  const [atmList, setAtmList] = useState<Atm[]>([])
+
+  const [district, setDistrict] = useState()
+  const [address, setAddress] = useState()
+  const [bankName, setBankName] = useState()
+
+  useEffect(() => {
+    const fn = async () => {
+      const { latest_record } = await listAtm({ district, address, bankName })
+      setAtmList(latest_record)
+    }
+    fn()
+  }, [district, address, bankName])
 
   useEffect(() => {
     if (ref.current && !map) {
@@ -17,5 +32,20 @@ export const App = () => {
     }
   }, [ref, map])
 
-  return <Box h="100vh" w="100vw" ref={ref} id="map" />
+  console.log(atmList)
+
+  return (
+    <Box h="100vh" w="100vw" ref={ref} id="map">
+      {atmList.map(({ item_id, latitude, longitude }) => (
+        <Marker
+          key={item_id}
+          position={{
+            lat: Number(latitude),
+            lng: Number(longitude),
+          }}
+          map={map}
+        />
+      ))}
+    </Box>
+  )
 }
