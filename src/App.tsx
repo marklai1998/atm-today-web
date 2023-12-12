0eaafc0da721feaea5512b1a38b0c74a1b7985a6
_ <_@_.com>
2023-12-13 00:14:45 +0800

    feat: call real API

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Box } from '@chakra-ui/react'
+import { Box, Flex, Input } from '@chakra-ui/react'
 import { useEffect, useRef, useState } from 'react'
 import { Atm, listAtm } from './services/listAtm'
 import { Marker } from './Marker'
@@ -9,9 +9,9 @@ export const App = () => {
   const [map, setMap] = useState<google.maps.Map>()
   const [atmList, setAtmList] = useState<Atm[]>([])
 
-  const [district, setDistrict] = useState()
-  const [address, setAddress] = useState()
-  const [bankName, setBankName] = useState()
+  const [district, setDistrict] = useState<string>()
+  const [address, setAddress] = useState<string>()
+  const [bankName, setBankName] = useState<string>()
 
   useEffect(() => {
     const fn = async () => {
@@ -35,21 +35,59 @@ export const App = () => {
   }, [ref, map])
 
   return (
-    <Box h="100vh" w="100vw" ref={ref} id="map">
-      {atmList.map(atm => {
-        const { item_id, latitude, longitude } = atm
-        return (
-          <Marker
-            key={item_id}
-            position={{
-              lat: Number(latitude),
-              lng: Number(longitude),
-            }}
-            map={map}
-            atm={atm}
-          />
-        )
-      })}
+    <Box>
+      <Flex
+        position="fixed"
+        w="50"
+        top="4"
+        left="4"
+        flexDirection="column"
+        gap="2"
+      >
+        <Input
+          bg="white"
+          size="small"
+          placeholder="address"
+          value={address}
+          onChange={e => {
+            setAddress(e.target.value)
+          }}
+        />
+        <Input
+          bg="white"
+          size="small"
+          placeholder="district"
+          value={district}
+          onChange={e => {
+            setDistrict(e.target.value)
+          }}
+        />
+        <Input
+          bg="white"
+          size="small"
+          placeholder="bank"
+          value={bankName}
+          onChange={e => {
+            setBankName(e.target.value)
+          }}
+        />
+      </Flex>
+      <Box h="100vh" w="100vw" ref={ref} id="map">
+        {atmList.map(atm => {
+          const { item_id, latitude, longitude } = atm
+          return (
+            <Marker
+              key={item_id}
+              position={{
+                lat: Number(latitude),
+                lng: Number(longitude),
+              }}
+              map={map}
+              atm={atm}
+            />
+          )
+        })}
+      </Box>
     </Box>
   )
 }
